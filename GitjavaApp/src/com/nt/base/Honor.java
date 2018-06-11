62854f6dc9d122a64c3fe60f2fc9bf5a38ae0b93
kriantl <kiran@gmail.com>
2018-06-11 16:14:51 +0530

    Modify and send me details----->kiranTL

---
+++ GitjavaApp/src/com/nt/base/Honor.java
@@ -0,0 +1,17 @@
+package com.nt.base;
+
+public class Honor {     	
+        
+	public static void main(String[] args) {
+		System.out.println("Honor Mobile Details");
+		{
+			Honor h=new Honor();
+		System.out.println("getHonorDetails:");
+		
+			
+			
+		}
+
+	}
+
+}
